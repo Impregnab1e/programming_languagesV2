9a47de1c59e2683218e70ab18224dc65a007d33f
Dmitriy <wuwka1@ya.ru>
2022-12-24 13:23:06 +0300

    task 1 rdy

---
+++ Sem2_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите трёхзначное число");
+int Numbers = int.Parse(Console.ReadLine()!);
+
+if (Numbers > 99 && Numbers < 1000)
+{
+    int SecondDigit = (Numbers / 10) % 10;
+    Console.WriteLine($"вторая цифра числа {Numbers} это {SecondDigit}");
+}
+else
+{
+    Console.Write("Введите другое число");
+}
