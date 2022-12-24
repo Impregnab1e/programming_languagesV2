d163f57590da9a91d025920b8ccfe88e3920ed90
Dmitriy <wuwka1@ya.ru>
2022-12-24 13:25:26 +0300

    2 Task rdy

---
+++ Sem2_Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int Numbers = int.Parse(Console.ReadLine()!);
+
+if (Numbers >= 100 && Numbers < 1000)
+{
+    int ThirdDigit = Numbers % 10;
+    Console.WriteLine($"третья цифра {Numbers} это {ThirdDigit}");
+}
+else if (Numbers >= 1000 && Numbers < 10000)
+{
+    int ThirdDigit = (Numbers / 10) % 10;
+    Console.WriteLine($"третья цифра {Numbers} это {ThirdDigit}");
+}
+else if (Numbers >= 10000 && Numbers < 100000)
+{
+    int ThirdDigit = (Numbers / 100) % 10;
+    Console.WriteLine($"третья цифра {Numbers} это {ThirdDigit}");
+}
+else if (Numbers >= 0 && Numbers <= 99)
+{
+    Console.Write("Третьей цифры нет");
+}
