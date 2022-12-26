2caef322eeed7a1a847c9fe2736abd5e10c56e65
Dmitriy <wuwka1@ya.ru>
2022-12-26 13:17:55 +0300

    Finish Sem3_Task2

--- Sem3_Task2/Program.cs
+++ Sem3_Task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int userAx = getUserValue("Введите X");
+int userAy = getUserValue("Введите Y");
+int userAz = getUserValue("Введите Z");
+
+int userBx = getUserValue("Введите X");
+int userBy = getUserValue("Введите Y");
+int userBz = getUserValue("Введите Z");
+
+double getDistanceCoordinate(int userAx, int userAy, int userAz, int userBx, int userBy, int userBz)
+{
+    double result = Math.Sqrt(Math.Pow((userAx - userBx), 2) + Math.Pow((userAy - userBy), 2) + Math.Pow((userAz - userBz), 2));
+    return result;
+}
+double distance = getDistanceCoordinate(userAx, userAy, userAz, userBx, userBy, userBz);
+Console.WriteLine($"Расстояние между точками в 3D пространстве это {Math.Round(distance,2)}");
+
+
