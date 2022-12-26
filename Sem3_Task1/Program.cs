b072101821239db96a67466e67b9365846a4c589
Dmitriy <wuwka1@ya.ru>
2022-12-26 12:47:17 +0300

    Finish Sem3_Task1

--- Sem3_Task1/Program.cs
+++ Sem3_Task1/Program.cs
@@ -3,3 +3,41 @@
 14212 -> нет
 12821 -> да
 23432 -> да */
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int N = getUserValue("Введите пятизначное число: ");
+
+
+int Palindrome(int N)
+{
+    int FirstNumber = (N / 10000) % 10;
+    int SecondNumber = (N / 1000) % 10;
+    int FourthNumber = (N / 10) % 10;
+    int FifthNumber = N % 10;
+
+    if (N > 9999 && N < 100000)
+    {
+        if (FirstNumber == FifthNumber && SecondNumber == FourthNumber)
+        {
+            Console.WriteLine($"Число {N} является палиндромом!");
+        }
+        else
+        {
+            Console.WriteLine($"Число {N} не является палиндромом!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите другое число");
+    }
+return N;
+}
+Palindrome(N);
+
