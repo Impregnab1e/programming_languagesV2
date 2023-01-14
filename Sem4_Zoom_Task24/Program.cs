8376f540218e56d5676661e5b217e9802e3c9714
Dmitriy <wuwka1@ya.ru>
2023-01-14 20:50:51 +0300

    add Sem4_zoom_Task30

--- Sem4_Zoom_Task24/Program.cs
+++ Sem4_Zoom_Task24/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 24. Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+7 > 28;
+4 > 10;
+8 > 36.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(mpessage);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getSumOfRange (int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i; // sum = sum +1;
+    }
+    return sum;
+}
