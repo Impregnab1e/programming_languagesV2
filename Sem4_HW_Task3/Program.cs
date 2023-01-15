4a478565db9262aa900ddf0b9004e9f1df6dab0e
Dmitriy <wuwka1@ya.ru>
2023-01-15 21:03:59 +0300

    finish Sem4_HW123

--- Sem4_HW_Task3/Program.cs
+++ Sem4_HW_Task3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Console.Clear();
+
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент под индексом {i} ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+
+
+void showArray(int[] array)
+{
+    Console.Write($"[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1)
+        {
+            Console.Write($"{array[i]},");
+        }
+        else
+        {
+            Console.Write($"{array[i]}]");
+        }
+    }
+}
+
+showArray(array);
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.Clear ();
+﻿Console.Clear();
 
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент под индексом {i} ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+
+Console.WriteLine($"Вывод массива: {array} ");
